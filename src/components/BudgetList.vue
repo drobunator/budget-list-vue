6da62062e73e4cb377079f64e55faea7e14eb0b2
Alex Bor <drobunator@gmail.com>
2020-04-18 20:47:57 +0300

    first commit

---
+++ src/components/BudgetList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="budget-list-wrapp">
+    <el-card :header="header" class="card">
+      <div class="list-item" v-for="(item, prop) in list" :key="prop">
+        <span class="budget-comment">{{ item.comment }}</span>
+        <span class="budget-value">{{ item.value }}</span>
+        <el-button type="danger" size="mini">Delete</el-button>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BudgetList',
+  props: {
+    list: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data: () => ({
+    header: 'Budget List'
+  })
+}
+</script>
+
+
+<style scoped>
+.card {
+  max-width: 768px;
+  margin: 0 auto;
+}
+.list-item {
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+}
+.budget-value {
+  font-weight: bold;
+  margin-left: auto;
+  margin-right: 20px;
+}
+</style>
